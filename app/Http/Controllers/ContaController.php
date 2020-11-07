6227eff488e46109f34307e2666134588148a943
Natália Lopes Oliveira <onatalia86@gmail.com>
2020-11-07 09:23:26 -0300

    Anuncio e Conta

---
+++ app/Http/Controllers/ContaController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Conta;
+
+class ContaController extends Controller
+{
+    public function create(Request $request)
+    {
+
+        $conta= new Conta;
+        $conta->nome= $request->nome;
+        $conta->email= $request->email;
+        $conta->empresa= $request->empresa;
+        $conta->segmento= $request->segmento;
+        $conta->estado= $request->estado;
+        $conta->senha= $request->senha;
+
+        $conta->save();
+
+        // return redirect('/...');
+    }
+
+    public function list()
+    {
+
+        $contas = Contas::all();
+
+        // return view('....', compact('contas'));
+    }
+}

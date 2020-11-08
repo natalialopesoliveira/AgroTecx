6b73daaf0706ce26078e39a26a5067683acfeb0b
André Luiz Brandão <andreluizbrandao31@gmail.com>
2020-11-07 21:50:38 -0300

    image handler api

--- routes/api.php
+++ routes/api.php
@@ -26,26 +26,26 @@ $router->group(['prefix' => 'user'], function () use ($router) {
 
 $router->group(['prefix' => 'product'], function () use ($router) {
 
-    // $router->get('/', 'ProductController@index');
+    $router->get('/', 'ProductController@index');
     $router->post('/', 'ProductController@store');
     $router->get('/{id}', 'ProductController@show');
     $router->put('/{id}', 'ProductController@update');
     $router->delete('/{id}', 'ProductController@destroy');
-    $router->post('/pay', 'ProductController@pay');
+    $router->post('/{$id}/pay', 'ProductController@pay');
 });
 
-// $router->group(['prefix' => 'bookmark'], function () use ($router) {
+$router->group(['prefix' => 'bookmark'], function () use ($router) {
 
-//     $router->post('/', 'BookmarkController@store');
-//     $router->get('/{id}', 'BookmarkController@show');
-//     $router->put('/{id}', 'BookmarkController@update');
-//     $router->delete('/{id}', 'BookmarkController@destroy');
-// });
+     $router->post('/', 'BookmarkController@store');
+     $router->get('/{id}', 'BookmarkController@show');
+     $router->put('/{id}', 'BookmarkController@update');
+     $router->delete('/{id}', 'BookmarkController@destroy');
+});
 
-// $router->group(['prefix' => 'credit'], function () use ($router) {
-//     $router->post('/', 'CreditCardController@store');
-//     $router->get('/{id}', 'CreditCardController@show');
-//     $router->put('/{id}', 'CreditCardController@update');
-//     $router->delete('/{id}', 'CreditCardController@destroy');
-//     $router->post('/check', 'CreditCardController@check');
-// });
+ $router->group(['prefix' => 'credit'], function () use ($router) {
+     $router->post('/', 'CreditCardController@store');
+     $router->get('/{id}', 'CreditCardController@show');
+     $router->put('/{id}', 'CreditCardController@update');
+     $router->delete('/{id}', 'CreditCardController@destroy');
+     $router->post('/check', 'CreditCardController@check');
+});

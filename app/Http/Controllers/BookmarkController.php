e7ea0b74e016fac1e4d7eec972b4442214762dab
luanloose <luanloose@hotmail.com>
2020-11-08 02:52:28 -0300

    ajustes card controller

--- app/Http/Controllers/BookmarkController.php
+++ app/Http/Controllers/BookmarkController.php
@@ -3,98 +3,72 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Bookmark;
-use App\Products;
+use App\User;
 
 class BookmarkController extends Controller
 {
-    // private $bookmark;
-    //
-    // /**
-    // * Create a new bookmark instance.
-    // *
-    // * @return void
-    // */
-    // public function __construct(Bookmark $bookmark)
-    // {
-    //     $this->bookmark = $bookmark;
-    // }
+    private $user;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
 
     public function store($user_id, Request $request)
     {
-        $validatedData = $request->validate([
-            'user_id' => 'required',
-            'product_id' => 'required'
-        ]);
-
-        $user = User::find($request->user_id);
+        $user = $this->user->find($request->user_id);
 
         try {
 
-            $bookmark = $user->bookmark()->attach($request->product_id);
+            $user->bookmark()->attach($request->product_id);
 
             return response()->json(
                 "Produto favoritado com sucesso!",
                 200
             );
         } catch (\Exception $e) {
-            return response()->json(
-                "Ocorreu um erro ao favoritar produto, contate o administrador",
-                500
-            );
+            return response()->json("Ocorreu um erro ao favoritar produto, contate o administrador", 500);
         }
-
     }
 
     public function show($user_id)
     {
         try {
 
-            $user = User::find($user_id);
+            $user = $this->user->find($user_id);
 
             $product = $user->bookmark;
 
             if ($product) {
-                return response(new ProductResource($product));
+                return response($product);
             }
 
-            return response()->json(
-                "Não há produtos favoritados",
-                404
-            );
+            return response()->json("Não há produtos favoritados", 404);
         } catch (\Exception $e) {
-            return response()->json(
-                "Ocorreu um erro na busca, contate o administrador",
-                500
-            );
+            return response()->json("Ocorreu um erro na busca, contate o administrador", 500);
         }
-
     }
 
     public function destroy($user_id, $product_id)
     {
-        $user = User::find($user_id);
+        $user = $this->user->find($user_id);
 
         $user->bookmark()->detach($product_id);
 
-        return response()->json(
-            "Produto foi desfavoritado com sucesso!",
-            200
-        );
-
+        return response()->json("Produto foi desfavoritado com sucesso!", 200);
     }
 
     public function destroyAll($user_id)
     {
-        $user = User::find($user_id);
+        $user = $this->user->find($user_id);
 
         $user->bookmark()->detach();
 
-        return response()->json(
-            "Os favoritos foram eliminados com sucesso!",
-            200
-        );
-
+        return response()->json("Os favoritos foram eliminados com sucesso!", 200);
     }
-
 }
--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,86 +3,70 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Cart;
+use App\User;
 
 class CartController extends Controller
 {
+    private $user;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
 
     public function store(Request $request)
     {
-        $validatedData = $request->validate([
-            'user_id' => 'required',
-            'product_id' => 'required'
-        ]);
-
-        $user = User::find($request->user_id);
 
         try {
 
-            $cart = $user->cart()->attach($request->product_id);
+            $user = $this->user->find($request->user_id);
 
-            return response()->json(
-                "Produto inserido no carrinho com sucesso!",
-                200
-            );
+            $user->cart()->attach($request->product_id);
+
+            return response()->json("Produto inserido no carrinho com sucesso!", 200);
         } catch (\Exception $e) {
-            return response()->json(
-                "Ocorreu um erro ao inserir produto no carrinho, contate o administrador",
-                500
-            );
+            return response()->json("Ocorreu um erro ao inserir produto no carrinho, contate o administrador", 500);
         }
-
     }
 
     public function show($user_id)
     {
         try {
 
-            $user = User::find($user_id);
+            $user = $this->user->find($user_id);
 
             $product = $user->cart;
 
             if ($product) {
-                return response(new ProductResource($product));
+                return response($product);
             }
 
-            return response()->json(
-                "Não há produtos no carrinho",
-                404
-            );
+            return response()->json("Não há produtos no carrinho", 404);
         } catch (\Exception $e) {
-            return response()->json(
-                "Ocorreu um erro na busca, contate o administrador",
-                500
-            );
+            return response()->json("Ocorreu um erro na busca, contate o administrador", 500);
         }
-
     }
 
     public function destroy($user_id, $id_product)
     {
-        $user = User::find($user_id);
+        $user = $this->user->find($user_id);
 
         $user->cart()->detach($id_product);
 
-        return response()->json(
-            "Produto foi removido do carrinho com sucesso!",
-            200
-        );
-
+        return response()->json("Produto foi removido do carrinho com sucesso!", 200);
     }
 
     public function destroyAll($user_id)
     {
-        $user = User::find($user_id);
+        $user = $this->user->find($user_id);
 
         $user->cart()->detach();
 
-        return response()->json(
-            "O carrinho foi esvaziado com sucesso!",
-            200
-        );
-
+        return response()->json("O carrinho foi esvaziado com sucesso!", 200);
     }
-
 }
